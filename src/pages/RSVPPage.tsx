b6a846e5f2162f25e30f0275b9b3d35cc68ec603
Julija Dobilaitė <julijadobilaite@MacBookPro.lan>
2026-05-18 01:42:33 +0300

    Atsakymų saugojimo taisymas

--- src/pages/RSVPPage.tsx
+++ src/pages/RSVPPage.tsx
@@ -42,7 +42,7 @@ function RSVPPage() {
     {
         method: "POST",
         headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "text/plain;charset=utf-8",
         },
         body: JSON.stringify(formData),
     }
